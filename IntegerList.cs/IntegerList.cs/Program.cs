cca7235afc238c1f2bda3822fce3bd72de06b9dd
MuayMal <mtoomer81@gmail.com>
2014-03-27 10:11:36 -0400

    hgdutycjy

---
+++ IntegerList.cs/IntegerList.cs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num = new int[5] { 3, 4, 1, 2, 5 };
+
+            for (int n = 0; n < 5; n++)
+            {
+                Array.Sort(num);
+                Console.Write(num[n] + " ");
+                
+            }
+            for(int i = 0; i < 5; i++)
+            {
+             
+                Array.Sort(num);
+   
+              Array.Reverse(num);
+               Console.Write(num[i] + " ");
+            }
+
+            
+            
+        }
+    }
+}
---
+++ IntegerList.cs/ZipCode.cs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZipCode.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zip = new int[10] {18042, 36548, 98624, 13597, 24584, 36489, 36897, 21549, 12654, 15034};
+
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(zip[i]);
+            }
+
+            Console.WriteLine();
+             */
+
+            Console.Write("Enter one of the listed zipcodes: ");
+            String input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+
+            for (int x = 0; x < zip.Length; x++)
+            {
+                if (n == zip[x])
+                {
+                    Console.WriteLine("Your package will be there in 5-7 buisness days");
+                }
+
+               else if (n != zip[x])
+                {
+                    Console.WriteLine("Sorry we can't ship it there");
+                }
+             
+            }
+
+
+
+        }
+    }
+}
